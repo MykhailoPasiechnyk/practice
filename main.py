9a9f0b2740141718b0c661d1dc66f53106f53ecf
MykhailoPasiechnyk <pasiechnyk.ms@gmail.com>
2023-03-15 17:42:19 +0100

    create func get_time_log, add format for logging and run function in main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from kubernetes import client, config
 from datetime import datetime
+import logging
+
+FORMAT = '%(asctime)s - %(message)s'
 
 
 def get_pod_age(pod_data):
@@ -10,6 +13,11 @@ def get_pod_age(pod_data):
     return day_minutes - pod_start_time_in_minutes
 
 
+def get_time_log(pod_data, log_format, age):
+    logging.basicConfig(format=log_format, level=logging.INFO)
+    logging.info(f'Name: {pod_data.metadata.name}, labels: {pod_data.metadata.labels}, AGE: {age}min;')
+
+
 if __name__ == '__main__':
     config.load_config()
     v1 = client.CoreV1Api()
@@ -18,5 +26,5 @@ if __name__ == '__main__':
     for pod in list_pod.items:
         if 'env' in pod.metadata.labels.keys() and pod.metadata.labels['env'] == 'test':
             pod_age = get_pod_age(pod)
-            print(pod_age)
+            get_time_log(pod, FORMAT, pod_age)
         continue
